ff4c610062dc0002c00a7c17d9836f38a9fe9ad9
noahffiliation <nlindsey15@gmail.com>
2025-02-02 14:54:29 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+<h1 align="center">Smart Playlists</h1>
+
+## Table of Contents
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Usage](#usage)
+- [Built Using](#built_using)
+- [Authors](#authors)
+- [Acknowledgments](#acknowledgement)
+
+## About <a name = "about"></a>
+As a heavy Spotify user, I wanted to replicate iTunes' Smart Playlists feature in Spotify, specifically Recently Added since I add music to my Spotify library all the time. Because my playlists are rather large at this point (1,000+ tracks), recent tracks don't get played very often and get lost in the sauce. This script running periodically keeps an automatically updated playlist of recently added tracks to my library.
+
+## Getting Started <a name = "getting_started"></a>
+### Prerequisites
+- [Spotify Developer account](https://developer.spotify.com/)
+- An app created in the Spotify Developer Dashboard
+    - Enable `Web API` in the settings
+
+### Installing
+Create a Python Virtual Environment
+```
+python -m venv venv
+source venv/bin/activate
+```
+
+Install Python packages:
+```
+pip install spotipy python-dotenv
+```
+
+Create `.env`
+```
+touch .env
+```
+Add info for `CLIENT_ID` and `CLIENT_SECRET` from the Spotify Developer App Settings
+A good default for `REDIRECT_URI` is `http://localhost:8888/callback`, make sure it matches in the Spotify Developer App settings
+`SOURCE_PLAYLIST_ID` is from your public Spotify playlist share link
+`TARGET_PLAYLIST_NAME` is the name of your new playlist
+
+## Usage <a name="usage"></a>
+Once you fill out your `.env`, you can run the Python file to test it out
+Use either a cronjob or Windows Task Scheduler to run the script periodically
+
+## Built Using <a name = "built_using"></a>
+- [Python](https://www.python.org/)
+
+## Authors <a name = "authors"></a>
+- [@Noahffiliation](https://github.com/Noahffiliation) - Idea & Initial work
+
+## Acknowledgements <a name = "acknowledgement"></a>
+- iTunes
---
+++ smart_playlists.py
@@ -0,0 +1,75 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from datetime import datetime, timedelta
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = getenv('CLIENT_ID')
+CLIENT_SECRET = getenv('CLIENT_SECRET')
+REDIRECT_URI = getenv('REDIRECT_URI')  # Must match your Spotify app settings
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri=REDIRECT_URI,
+    scope='playlist-modify-public playlist-read-private'
+))
+
+def get_all_playlist_tracks(playlist_id):
+
+	# Fetch all tracks in the playlist, 100 at a time since that's the maximum limit for spotipy
+    tracks = []
+    offset = 0
+    limit = 100
+    while True:
+        results = sp.playlist_tracks(playlist_id, offset=offset, limit=limit)
+        tracks.extend(results['items'])
+        if results['next']:
+            offset += limit
+        else:
+            break
+
+    return tracks
+
+def get_recent_tracks(playlist_id):
+    tracks = get_all_playlist_tracks(playlist_id)
+
+    one_month_ago = datetime.now() - timedelta(days=30)
+    print(f"One month ago: {one_month_ago}")
+
+    recent_tracks = []
+    for item in tracks:
+        added_at = datetime.strptime(item['added_at'], '%Y-%m-%dT%H:%M:%SZ')
+        if added_at > one_month_ago:
+            recent_tracks.append(item['track']['uri'])
+
+    return recent_tracks
+
+def update_recent_tracks_playlist(source_playlist_id, target_playlist_name):
+    recent_tracks = get_recent_tracks(source_playlist_id)
+
+    playlists = sp.current_user_playlists()['items']
+    target_playlist = next((p for p in playlists if p['name'] == target_playlist_name), None)
+
+    if target_playlist:
+        sp.playlist_replace_items(target_playlist['id'], [])
+    else:
+        user_id = sp.current_user()['id']
+        target_playlist = sp.user_playlist_create(user_id, target_playlist_name, public=True)
+
+    if recent_tracks:
+
+		# List newest tracks first
+        recent_tracks.reverse()
+        sp.playlist_add_items(target_playlist['id'], recent_tracks)
+        print(f"Updated playlist '{target_playlist_name}' with {len(recent_tracks)} tracks.")
+    else:
+        print("No recent tracks found to add.")
+
+if __name__ == "__main__":
+    SOURCE_PLAYLIST_ID = getenv('SOURCE_PLAYLIST_ID')
+    TARGET_PLAYLIST_NAME = getenv('TARGET_PLAYLIST_NAME')
+
+    update_recent_tracks_playlist(SOURCE_PLAYLIST_ID, TARGET_PLAYLIST_NAME)
